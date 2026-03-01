23ef082c7b9f5b165c47251e4e937c913a0fcb9c
rei-ebihara <157778172+rei-ebihara@users.noreply.github.com>
2026-03-01 15:10:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-let score = 0
+let count = 0
+let bomb = Math.floor(Math.random() * 10) + 5 // 5〜14回で爆発
 
-function tap() {
-  score++
-  document.getElementById("score").textContent = score
+function push() {
+  if (bomb === -1) return
+
+  count++
+  document.getElementById("count").textContent = count
+
+  if (count === bomb) {
+    document.getElementById("status").textContent = "💥 爆発！"
+    bomb = -1
+  }
 }
